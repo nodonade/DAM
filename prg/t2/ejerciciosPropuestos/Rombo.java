b15c9e9f70eeca820fbef02391f81811adbf3b91
nodonade <nodonade@gmail.com>
2019-10-05 19:33:39 +0200

    automatic commit

--- prg/t2/ejerciciosPropuestos/Rombo.java
+++ prg/t2/ejerciciosPropuestos/Rombo.java
@@ -14,25 +14,27 @@ public class Rombo {
 		Scanner kbd = new Scanner(System.in);
 
 		System.out.printf("Deme la anchura del rombo: ");
-		int anchura = kbd.nextInt();
+		int size = kbd.nextInt();
 
-		for (int i = 0; i < anchura; i++) {
-			for(int j = anchura - i; j > 0; j--) {
-				System.out.printf(" ");
-			}
-			for (int j = 2 * i -1; j > 0; j--) {
-				System.out.printf("*");
-			}
-			System.out.printf("\n");
-		}
-		for (int i = anchura - 1; i >= 0; i--) {
-			for (int j = 0; j < anchura - i; j++) {
-				System.out.printf(" ");
-			}
-			for (int j = 0; j < 2*i-1; j++) {
-				System.out.printf("*");
-			}
-			System.out.printf("\n");
-		}
+
+		    for (int i = 1; i < size; i += 2) {
+		        for (int k = size; k >= i; k -= 2) {
+        		    System.out.print(" ");
+       			 }
+  		     	 for (int j = 1; j <= i; j++) {
+            			System.out.print("*");
+		        }
+		        System.out.println();
+		    }// end loop
+
+		    for (int i = 1; i <= size; i += 2) {
+		        for (int k = 1; k <= i; k += 2) {
+		            System.out.print(" ");
+		        }
+		        for (int j = size; j >= i; j--) {
+		            System.out.print("*");
+		        }
+	        System.out.println();
+		}// end loop
 	}
 }
